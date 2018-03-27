8e0e54ba77d431931114138ef5b6c6e6e655f2ae
jpfaria19 <jpfaria19@hotmail.com>
2018-03-27 19:53:03 -0300

    Console app adicionando na mão, porém getall funcionando.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,48 +1,43 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
-//using DomainModel;
-//using System.Net.Http.Headers;
-//using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DomainModel;
+using System.Net.Http.Headers;
+using System.Net.Http;
 
-//namespace ConsoleApp
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            HttpClient client = new HttpClient();
-//            client.BaseAddress = new Uri("http://localhost:51639/");
-//            client.DefaultRequestHeaders.Accept.Add(
-//                new MediaTypeWithQualityHeaderValue("application/json"));
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:51639/");
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
 
-//            //####### Friends Seed ########
-//            Friend friend1 = new Friend();
-//            friend1.Name = "Diego";
-//            friend1. = 20;
-//            //#############################
+            //####### Friends Seed ########
+            Friend friend1 = new Friend();
+            friend1.Name = "Samii";
+            friend1.SurName = "Portuga";
+            friend1.Email = "portuga.samii@psamii.com";
+            friend1.Phone = 987609803;
+            friend1.Birthday = new DateTime(2001, 7, 21);
+            //#############################
 
-//            Task<HttpResponseMessage> response = client.PostAsJsonAsync("api/friends", friend1);
-//            if (response.Result.IsSuccessStatusCode)
-//                Console.WriteLine($"Amigo {friend1.Name} incluido com sucesso!");
+            Task<HttpResponseMessage> response = client.PostAsJsonAsync("api/friends", friend1);
+            if (response.Result.IsSuccessStatusCode)
+                Console.WriteLine($"Amigo {friend1.Name} incluido com sucesso!");
 
-//            Console.WriteLine("Vamos imprimir todo mundo agora:");
-//            Task<HttpResponseMessage> response2 = client.GetAsync("api/friends");
-//            Task<IEnumerable<Friend>> taskFriends = response2.Result.Content.ReadAsAsync<IEnumerable<Friend>>();
-//            IEnumerable<Friend> friends = taskFriends.Result;
+            Console.WriteLine("Vamos imprimir todo mundo agora:");
+            Task<HttpResponseMessage> response2 = client.GetAsync("api/friends");
+            Task<IEnumerable<Friend>> taskFriends = response2.Result.Content.ReadAsAsync<IEnumerable<Friend>>();
+            IEnumerable<Friend> friends = taskFriends.Result;
 
-//            foreach(var f in friends)
-//                Console.WriteLine(f.Name);
+            foreach (var f in friends)
+                Console.WriteLine(f.Name);
 
-//            Console.ReadLine();
-//        }
-
-//        //public static async Task<HttpResponseMessage> AddFriend(HttpClient client, Friend friend)
-//        //{
-//        //    HttpResponseMessage response;
-//        //    response = await client.PostAsJsonAsync("api/friends", friend);
-
-//        //    return response;
-//        //}
-//    }
-//}
+            Console.ReadLine();
+        }
+    }
+}
